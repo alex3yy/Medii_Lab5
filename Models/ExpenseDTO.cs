9d24feab7a770021c0d8cb497b6e1738a829b628
Alex Zaharia <alex_zaharia10@yahoo.com>
2021-11-09 19:49:50 +0200

    Add project files.

---
+++ Models/ExpenseDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zaharia_Alexandru_Lab5.Models {
+
+    public class ExpenseDTO {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
